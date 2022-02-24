564142f782a87b14f735ad5518a264f24c11628a
Marek Hanzal <marek.hanzal@x32.cz>
2022-02-24 01:39:57 +0100

    Common: Added interface from sdk

---
+++ src/ts/interface.ts
@@ -0,0 +1,15 @@
+import type ts from "typescript";
+
+export interface INode {
+	node: ts.Node;
+	sourceFile: ts.SourceFile;
+	syntaxKind: string;
+	source: string;
+}
+
+export type INodePath = {
+	path: string;
+	node: ts.Node;
+};
+
+export type IForeachNodeCallback = (node: INode) => void;
