c0ed16b80b9816bcd65e55666421bbd2b11215c6
Marek Hanzal <marek.hanzal@x32.cz>
2022-03-01 03:51:41 +0100

    Common: Improved link

--- src/link/interface.ts
+++ src/link/interface.ts
@@ -1,7 +1,5 @@
 export type IQueryParams = { [key: string]: string | string[] } | void;
 
 export interface ILinkContext {
-	generate<TQuery extends IQueryParams = IQueryParams>(href: string, query?: TQuery): string;
-
 	link<TQuery extends IQueryParams = IQueryParams>(href: string, query?: TQuery): string;
 }
