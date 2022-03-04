cc53656dbf1423572654d8d147edcd420f3059b9
Marek Hanzal <marek.hanzal@x32.cz>
2022-03-04 23:07:15 +0100

    Common: A little tuning

--- src/file/chunk.ts
+++ src/file/chunk.ts
@@ -25,4 +25,4 @@ export interface IChunkService {
 
 export type IChunkServiceFactory = (deps: IChunkServiceDeps) => IChunkService;
 
-export type IChunkCommitEvent = <TRequest, TResponse, TQuery extends IQueryParams = IQueryParams>(file: IFile, params: IEndpointParams<TRequest, TResponse, TQuery>) => IFile;
+export type IChunkCommitEvent = <TRequest, TResponse, TQuery extends IQueryParams = IQueryParams>(file: IFile, params: IEndpointParams<TRequest, TResponse, TQuery>) => Promise<IFile>;
