14a500f7a7f66186d25f6c7d8f44e3b513904f81
Marek Hanzal <marek.hanzal@x32.cz>
2022-03-10 15:25:10 +0100

    Common: Extended IJob stuff

--- src/job/interface.ts
+++ src/job/interface.ts
@@ -1,3 +1,14 @@
 export interface IJob {
 	id: string;
+	agenda: string;
+	status: IJobStatus;
+	total: number;
+	success?: number;
+	successRatio?: number;
+	failure?: number;
+	failureRatio?: number;
+	created: Date;
+	userId: string;
 }
+
+export type IJobStatus = 'NEW' | 'RUNNING' | 'SUCCESS' | 'FAILURE' | 'DONE';
