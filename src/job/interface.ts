d21357974300b726bd5ad04faf6749a09657eb48
Marek Hanzal <marek.hanzal@x32.cz>
2022-03-10 15:44:23 +0100

    Common: Version bump

--- src/job/interface.ts
+++ src/job/interface.ts
@@ -3,12 +3,12 @@ export interface IJob {
 	agenda: string;
 	status: IJobStatus;
 	total: number;
-	success?: number;
-	successRatio?: number;
-	failure?: number;
-	failureRatio?: number;
+	success?: number | null;
+	successRatio?: number | null;
+	failure?: number | null;
+	failureRatio?: number | null;
 	created: Date;
-	userId?: string;
+	userId?: string | null;
 }
 
 export type IJobStatus = 'NEW' | 'RUNNING' | 'SUCCESS' | 'FAILURE' | 'DONE';
