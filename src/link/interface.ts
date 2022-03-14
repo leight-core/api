1d5aa3fa19b7e466a0f75e4414b7f24d3f89ee6b
Marek Hanzal <marek.hanzal@x32.cz>
2022-03-14 17:03:33 +0100

    Common: ...

--- src/link/interface.ts
+++ src/link/interface.ts
@@ -6,7 +6,7 @@ export interface ILinkContext {
 	link<TQuery extends IQueryParams | void = void>(href: string, query?: TQuery): string;
 }
 
-export interface IQueryParamsContext<TQueryParams = IQueryParams> {
+export interface IQueryParamsContext<TQueryParams extends IQueryParams> {
 	readonly queryParams?: TQueryParams;
 
 	setQueryParams(queryParams?: TQueryParams): void;
