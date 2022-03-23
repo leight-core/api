d3f147fc4d169ed2f6a39ac33cc51611642cec56
Marek Hanzal <marek.hanzal@x32.cz>
2022-03-23 17:31:58 +0100

    Common: ...

--- src/repository/interface.ts
+++ src/repository/interface.ts
@@ -1,4 +1,10 @@
 import {IImportHandlers, IPrismaClientTransaction, IQuery, IQueryResult} from "@leight-core/api";
+import {GetServerSideProps} from "next";
+import {ParsedUrlQuery} from "querystring";
+
+export interface IPageRepositoryService<TProps, TQueryParams extends ParsedUrlQuery> {
+	fetch(key: keyof TProps, query: keyof TQueryParams): GetServerSideProps<TProps, TQueryParams>;
+}
 
 export interface IRepositoryService<TCreate, TEntity, TResponse, TQuery extends IQuery<any, any>> {
 	create(create: TCreate): Promise<TEntity>;
@@ -14,6 +20,8 @@ export interface IRepositoryService<TCreate, TEntity, TResponse, TQuery extends 
 	list(entities: Promise<TEntity[]>): Promise<TResponse[]>;
 
 	importers(): IImportHandlers;
+
+	pageService: IPageRepositoryService<any, any>;
 }
 
 export type IRepositoryServiceFactory<TCreate, TEntity, TResponse, TQuery extends IQuery<any, any>> = (prisma?: IPrismaClientTransaction) => IRepositoryService<TCreate, TEntity, TResponse, TQuery>;
