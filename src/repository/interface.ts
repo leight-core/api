af0feab984ae634a28062ac9ac3fb8228b23123a
Marek Hanzal <marek.hanzal@x32.cz>
2022-03-23 18:10:58 +0100

    Common: Version bump

--- src/repository/interface.ts
+++ src/repository/interface.ts
@@ -2,7 +2,7 @@ import {IImportHandlers, IPrismaClientTransaction, IQuery, IQueryResult} from "@
 import {GetServerSideProps} from "next";
 import {ParsedUrlQuery} from "querystring";
 
-export interface IRepositoryService<TCreate, TEntity, TResponse, TQuery extends IQuery<any, any>> {
+export interface IRepositoryService<TCreate, TEntity, TResponse, TQuery extends IQuery<any, any>, TPageFetchProps, TPageFetchQueryParams extends ParsedUrlQuery> {
 	create(create: TCreate): Promise<TEntity>;
 
 	query(query: TQuery): Promise<IQueryResult<TResponse>>;
@@ -17,7 +17,7 @@ export interface IRepositoryService<TCreate, TEntity, TResponse, TQuery extends 
 
 	importers(): IImportHandlers;
 
-	pageFetch?<TProps, TQueryParams extends ParsedUrlQuery>(key: keyof TProps, query: keyof TQueryParams): GetServerSideProps<TProps, TQueryParams>;
+	pageFetch(key: keyof TPageFetchProps, query: keyof TPageFetchQueryParams): GetServerSideProps<TPageFetchProps, TPageFetchQueryParams>;
 }
 
-export type IRepositoryServiceFactory<TCreate, TEntity, TResponse, TQuery extends IQuery<any, any>> = (prisma?: IPrismaClientTransaction) => IRepositoryService<TCreate, TEntity, TResponse, TQuery>;
+export type IRepositoryServiceFactory<TCreate, TEntity, TResponse, TQuery extends IQuery<any, any>, TPageFetchProps, TPageFetchQueryParams extends ParsedUrlQuery> = (prisma?: IPrismaClientTransaction) => IRepositoryService<TCreate, TEntity, TResponse, TQuery, TPageFetchProps, TPageFetchQueryParams>;
