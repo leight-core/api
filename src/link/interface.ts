396eff129599a3950e7bf98435977da882eb8ce6
Marek Hanzal <marek.hanzal@x32.cz>
2022-04-13 11:07:24 +0200

    Common: Removed link context

--- src/link/interface.ts
+++ src/link/interface.ts
@@ -1,9 +1,5 @@
 export interface IQueryParams {
-	[key: string]: string | string[]
-}
-
-export interface ILinkContext {
-	link<TQuery extends IQueryParams | undefined = undefined>(href: string, query?: TQuery): string;
+	[key: string]: string | string[];
 }
 
 export interface IQueryParamsContext<TQueryParams extends IQueryParams | undefined = undefined> {
