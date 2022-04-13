c276b57157b75664b03bf345be232e18d9cee853
Marek Hanzal <marek.hanzal@x32.cz>
2022-04-13 11:14:24 +0200

    Common: Version bump

--- src/source/interface.ts
+++ src/source/interface.ts
@@ -1,12 +1,13 @@
 import {IQueryParams} from "@leight-core/api";
 import {PaginationConfig} from "antd/es/pagination";
-import {UseMutationOptions, UseMutationResult, UseQueryResult} from "react-query";
 import {AxiosRequestConfig} from "axios";
+import {UseMutationOptions, UseMutationResult, UseQueryResult} from "react-query";
 import {UseQueryOptions} from "react-query/types/react/types";
 
 export type IQueryHook<TRequest, TResponse, TQueryParams extends IQueryParams | undefined = undefined> = (request?: TRequest, queryParams?: TQueryParams, options?: UseQueryOptions<any, any, TResponse, any>, config?: AxiosRequestConfig<TRequest>) => UseQueryResult<TResponse>;
 export type IMutationHook<TRequest, TResponse, TQueryParams extends IQueryParams | undefined = undefined> = (queryParams?: TQueryParams, options?: UseMutationOptions<TResponse, any, TRequest>, config?: AxiosRequestConfig<TRequest>) => UseMutationResult<TResponse, any, TRequest>;
-export type IHookCallback<TRequest, TResponse, TQueryParams extends IQueryParams | undefined = undefined> = () => (request?: TRequest, queryParams?: TQueryParams, config?: AxiosRequestConfig<TRequest>) => Promise<TResponse>;
+export type IPromiseCallback<TRequest, TResponse, TQueryParams extends IQueryParams | undefined = undefined> = (request?: TRequest, queryParams?: TQueryParams, config?: AxiosRequestConfig<TRequest>) => Promise<TResponse>;
+export type IHookCallback<TRequest, TResponse, TQueryParams extends IQueryParams | undefined = undefined> = () => IPromiseCallback<TRequest, TResponse, TQueryParams>;
 
 export interface IQuery<TFilter = undefined, TOrderBy = undefined> {
 	/** currently requested page */
