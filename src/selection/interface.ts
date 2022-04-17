83ea4b77c850eb7ce8cd4076c6eb80b6f4869a30
Marek Hanzal <marek.hanzal@x32.cz>
2022-04-17 20:30:59 +0200

    Common: Version bump

--- src/selection/interface.ts
+++ src/selection/interface.ts
@@ -3,7 +3,11 @@ export type ISelectionType = "none" | "single" | "multi";
 export interface ISelectionContext<TSelection> {
 	onSelect(id: string, selection: TSelection): void;
 
+	onSelectItem(item: TSelection & { id: string }): void;
+
 	isSelected(id: string): boolean;
+
+	isSelectedItem(item: TSelection & { id: string }): boolean;
 
 	asSelection(): { [index in string]: TSelection; };
 
