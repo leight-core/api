ccdc723143669aacc1c902bfc4f01da5937b1395
Marek Hanzal <marek.hanzal@x32.cz>
2022-04-17 19:49:40 +0200

    Common: Version bump

--- src/selection/interface.ts
+++ src/selection/interface.ts
@@ -1,5 +1,21 @@
+export type ISelectionType = "none" | "single" | "multi";
+export type ISelectionBoolType = "true/false" | "true/false/undefined" | "true/undefined";
+
+export const SelectionTrueFalse = {"true": false, "false": true, "undefined": true};
+export const SelectionTrueUndefined = {"true": undefined, "undefined": true};
+export const SelectionTrueFalseUndefined = {"undefined": true, "true": false, "false": undefined};
+export const SelectionBoolTypeMap: { [index in ISelectionBoolType]: (typeof SelectionTrueFalse | typeof SelectionTrueUndefined | typeof SelectionTrueFalseUndefined) } = {
+	"true/false": SelectionTrueFalse,
+	"true/false/undefined": SelectionTrueFalseUndefined,
+	"true/undefined": SelectionTrueUndefined,
+};
+
 export interface ISelectionContext<TSelection> {
-	onSelect(selection: TSelection): boolean | undefined;
+	onSelect(id: string, selection: TSelection): void;
+
+	isSelected(id: string): boolean | undefined;
+
+	asSelection(): { [index in string]: TSelection; };
 
 	toSelection(): TSelection[];
 }
