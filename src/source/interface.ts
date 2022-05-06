6d9e771486432705cd09887c1290ff3932409713
Marek Hanzal <marek.hanzal@x32.cz>
2022-05-06 16:26:52 +0200

    Common: Version bump

--- src/source/interface.ts
+++ src/source/interface.ts
@@ -61,11 +61,18 @@ export interface IFilterContext<TFilter = any> {
 	 * Currently set filtering.
 	 */
 	readonly filter: TFilter;
+	/**
+	 * When filtering from a form or any kind of source which is mapping filter,
+	 * this is the original request (for example from filter form or so.).
+	 *
+	 * It's useful to restore filter form with the current filter.
+	 */
+	readonly source: any;
 
 	/**
 	 * Set the given filter as current (thus replacing one already set).
 	 */
-	setFilter(filter?: TFilter): void;
+	setFilter(filter?: TFilter, source?: any): void;
 
 	/**
 	 * Apply the given filter, thus merging with the one already set, but replacing first-level properties (thus there is no deep-merge).
