4fe9a36447caa66c3e2869d19af0b6fdea0d6768
Marek Hanzal <marek.hanzal@x32.cz>
2022-05-23 11:23:54 +0200

    Common: Version bump

--- src/next.js/interface.ts
+++ src/next.js/interface.ts
@@ -1,4 +1,4 @@
 import {GetServerSideProps} from "next";
 import {ParsedUrlQuery} from "querystring";
 
-export type IToPage = <TToPage, TToPageQueryParams extends ParsedUrlQuery>(key: keyof TToPage, query: keyof TToPageQueryParams) => GetServerSideProps<TToPage, TToPageQueryParams>;
+export type IToPage<TToPage, TToPageQueryParams extends ParsedUrlQuery> = (key: keyof TToPage, query: keyof TToPageQueryParams) => GetServerSideProps<TToPage, TToPageQueryParams>;
