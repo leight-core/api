848f8e2fd9f2c1f75a0e8da0df87463ceb3cef0a
Marek Hanzal <marek.hanzal@x32.cz>
2022-05-23 11:39:44 +0200

    Common: Version bump

--- src/source/ISource.ts
+++ src/source/ISource.ts
@@ -1,6 +1,8 @@
-import {IQuery} from "@leight-core/api";
+import {IPromiseMapper, IQuery, IUser} from "@leight-core/api";
 
-export interface ISource<TCreate, TEntity, TQuery extends IQuery<any, any>> {
+export interface ISource<TCreate, TEntity, TItem, TQuery extends IQuery<any, any>> {
+	readonly mapper: IPromiseMapper<TEntity, TItem>;
+
 	/**
 	 * Creates a new entity by the given request.
 	 */
@@ -35,12 +37,28 @@ export interface ISource<TCreate, TEntity, TQuery extends IQuery<any, any>> {
 	 * Return count of items by the given query.
 	 */
 	count(query: TQuery): Promise<number>;
+
+	/**
+	 * Set the given user as a context; user requirement is based on the source's internals.
+	 */
+	withUser(user: IUser): ISource<TCreate, TEntity, TItem, TQuery>;
+
+	/**
+	 * Set custom mapper for this source.
+	 */
+	withMapper<TTarget>(mapper: IPromiseMapper<TEntity, TTarget>): ISource<TCreate, TEntity, TTarget, TQuery>;
+
+	/**
+	 * Sets default mapper of this source (if any).
+	 */
+	withDefaultMapper(): ISource<TCreate, TEntity, TItem, TQuery>;
 }
 
-export interface IWithSource<TCreate, TEntity, TQuery extends IQuery<any, any>> {
-	source: ISource<TCreate, TEntity, TQuery>;
+export interface IWithSource<TCreate, TEntity, TItem, TQuery extends IQuery<any, any>> {
+	source: ISource<TCreate, TEntity, TItem, TQuery>;
 }
 
-export type ISourceCreate<T> = T extends ISource<infer TCreate, any, any> ? TCreate : T;
-export type ISourceEntity<T> = T extends ISource<any, infer TEntity, any> ? TEntity : T;
-export type ISourceQuery<T> = T extends ISource<any, any, infer TQuery> ? TQuery : T;
+export type ISourceCreate<T> = T extends ISource<infer TCreate, any, any, any> ? TCreate : T;
+export type ISourceEntity<T> = T extends ISource<any, infer TEntity, any, any> ? TEntity : T;
+export type ISourceItem<T> = T extends ISource<any, any, infer TItem, any> ? TItem : T;
+export type ISourceQuery<T> = T extends ISource<any, any, any, infer TQuery> ? TQuery : T;
