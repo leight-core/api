1694fe93303d884eae660911ba259dfe079495b1
Marek Hanzal <marek.hanzal@x32.cz>
2022-05-30 18:29:43 +0200

    Common: Version bump

--- src/interface/interface.ts
+++ src/interface/interface.ts
@@ -1,18 +1,11 @@
-// /**
-//  * Basic record must have an ID, thus all records must be extended from this type.
-//  */
-// export interface IRecordItem {
-// 	readonly [index: string]: any;
-//
-// 	readonly id: string;
-// }
-//
-// export interface IndexOf<T> {
-// 	[index: string]: T;
-// }
-//
-// export type Unboxed<T> = T extends (infer U)[] ? U : T;
+export type RequiredKeys<T> = { [K in keyof T]-?: Record<string, unknown> extends { [P in K]: T[K] } ? never : K }[keyof T];
+
+export type OptionalKeys<T> = { [K in keyof T]-?: Record<string, unknown> extends { [P in K]: T[K] } ? K : never }[keyof T];
+
+export type PickRequired<T> = Pick<T, RequiredKeys<T>>;
+
+export type PickOptional<T> = Pick<T, OptionalKeys<T>>;
 
 export type Nullable<T> = { [P in keyof T]: T[P] | null };
 
-export type NullableOptional<T> = Nullable<Partial<T>>;
+export type NullableOptional<T> = PickRequired<T> & Nullable<PickOptional<T>>;
