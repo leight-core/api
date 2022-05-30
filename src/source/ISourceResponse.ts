a857e088734b1da4cb925a40ff741dc8d21f8f89
Marek Hanzal <marek.hanzal@x32.cz>
2022-05-30 15:18:09 +0200

    Common: Version bump

--- src/source/ISourceResponse.ts
+++ src/source/ISourceResponse.ts
@@ -1,9 +1,10 @@
-import {IQuery, ISource} from "@leight-core/api";
+import {ISource} from "@leight-core/api";
 
-export interface ISourceResponse<TSource extends ISource<any, any, any, IQuery<any, any>>> {
+export interface ISourceResponse<TSource extends ISource<any, any, any>> {
 	get: Awaited<ReturnType<TSource["get"]>>;
 	query: Awaited<ReturnType<TSource["query"]>>;
 	count: Awaited<ReturnType<TSource["count"]>>;
 	create: Awaited<ReturnType<TSource["create"]>>;
+	patch?: Awaited<ReturnType<TSource["patch"]>>;
 	delete: Awaited<ReturnType<TSource["delete"]>>;
 }
