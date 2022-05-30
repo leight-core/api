b8c4d0af652e58299e8aa64c3c25316c3aed9455
Marek Hanzal <marek.hanzal@x32.cz>
2022-05-30 18:09:41 +0200

    Common: Version bump

--- src/link/interface.ts
+++ src/link/interface.ts
@@ -1,9 +1,7 @@
-export interface IQueryParams {
-	[key: string]: string | string[];
-}
+export type IQueryParams = Record<string, string | string[] | number | number[] | null | undefined>;
 
 export interface IQueryParamsContext<TQueryParams extends IQueryParams | undefined = undefined> {
 	readonly queryParams: TQueryParams;
 
-	setQueryParams(queryParams: TQueryParams): void;
+	setQueryParams(queryParams?: TQueryParams): void;
 }
