af539aa4c7a1eef21693ae1a3bbae1ee06bfba02
Marek Hanzal <marek.hanzal@x32.cz>
2022-06-07 16:38:20 +0200

    Common: Version bump

--- src/source/interface.ts
+++ src/source/interface.ts
@@ -3,15 +3,19 @@ import {AxiosRequestConfig} from "axios";
 import {UseMutationOptions, UseMutationResult, UseQueryResult} from "react-query";
 import {UseQueryOptions} from "react-query/types/react/types";
 
-export type IQueryHook<TRequest, TResponse, TQueryParams extends IQueryParams | undefined = undefined> = (request?: TRequest, queryParams?: TQueryParams, options?: UseQueryOptions<any, any, TResponse, any>, config?: AxiosRequestConfig<TRequest>) => UseQueryResult<TResponse>;
-export type IMutationHook<TRequest, TResponse, TQueryParams extends IQueryParams | undefined = undefined> = (queryParams?: TQueryParams, options?: UseMutationOptions<TResponse, any, TRequest>, config?: AxiosRequestConfig<TRequest>) => UseMutationResult<TResponse, any, TRequest>;
-export type IPromiseCallback<TRequest, TResponse, TQueryParams extends IQueryParams | undefined = undefined> = (request?: TRequest, queryParams?: TQueryParams, config?: AxiosRequestConfig<TRequest>) => Promise<TResponse>;
-export type IHookCallback<TRequest, TResponse, TQueryParams extends IQueryParams | undefined = undefined> = () => IPromiseCallback<TRequest, TResponse, TQueryParams>;
+export type IQueryHook<TRequest, TResponse, TQueryParams extends IQueryParams = any> = (request?: TRequest, queryParams?: TQueryParams, options?: UseQueryOptions<any, any, TResponse, any>, config?: AxiosRequestConfig<TRequest>) => UseQueryResult<TResponse>;
+export type IMutationHook<TRequest, TResponse, TQueryParams extends IQueryParams = any> = (queryParams?: TQueryParams, options?: UseMutationOptions<TResponse, any, TRequest>, config?: AxiosRequestConfig<TRequest>) => UseMutationResult<TResponse, any, TRequest>;
+export type IPromiseCallback<TRequest, TResponse, TQueryParams extends IQueryParams = any> = (request?: TRequest, queryParams?: TQueryParams, config?: AxiosRequestConfig<TRequest>) => Promise<TResponse>;
+export type IHookCallback<TRequest, TResponse, TQueryParams extends IQueryParams = any> = () => IPromiseCallback<TRequest, TResponse, TQueryParams>;
 
 export interface IWithFulltext {
 	fulltext?: string;
 }
 
 export interface IWithIdentity {
+	id: string;
+}
+
+export interface IWithIdentityQuery extends IQueryParams {
 	id: string;
 }
