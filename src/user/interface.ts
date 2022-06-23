afe0937242ed80bd9d7a6b1879aaf4aa79827325
Marek Hanzal <marek.hanzal@x32.cz>
2022-06-23 18:41:39 +0200

    Common: Version bump

--- src/user/interface.ts
+++ src/user/interface.ts
@@ -1,3 +1,5 @@
+import {ClientError} from "@leight-core/api";
+
 export interface IUser {
 	readonly tokens: string[];
 
@@ -24,4 +26,21 @@ export interface IUser {
 	 * Passes if user has all specified tokens.
 	 */
 	checkTokens(tokens?: string[]): void;
+}
+
+export class UndefinedUserError extends ClientError {
+	constructor(message: string) {
+		super(message, 403);
+	}
+}
+
+export class AclError extends ClientError {
+	readonly tokens: string[];
+	readonly requested?: string[];
+
+	constructor(message: string, tokens: string[], requested?: string[]) {
+		super(message, 403);
+		this.tokens = tokens;
+		this.requested = requested;
+	}
 }
