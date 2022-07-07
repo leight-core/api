7b7eab9825d87d120d71eecb0963e248eab73349
Marek Hanzal <marek.hanzal@x32.cz>
2022-07-07 20:22:09 +0200

    Common: Version bump

--- src/source/ICursorContext.ts
+++ src/source/ICursorContext.ts
@@ -1,13 +1,24 @@
+import {PaginationConfig} from "antd/es/pagination";
+import {UseQueryResult} from "react-query";
+
 export interface ICursorContext {
 	readonly name: string;
 	/**
 	 * Current page.
 	 */
-	readonly page?: number;
+	readonly page: number;
 	/**
 	 * Current page size.
 	 */
-	readonly size?: number;
+	readonly size: number;
 
-	setPage(page?: number, size?: number): void;
+	readonly count: UseQueryResult<number, any> | undefined;
+
+	pagination(): PaginationConfig | false | undefined;
+
+	setPage(page: number, size: number): void;
+
+	more(): void;
+
+	hasMore(): boolean;
 }
--- src/source/ISourceContext.ts
+++ src/source/ISourceContext.ts
@@ -1,16 +1,9 @@
-import {PaginationConfig} from "antd/es/pagination";
 import {UseQueryResult} from "react-query";
 
 export interface ISourceContext<TResponse> {
 	readonly name: string;
 
 	readonly result: UseQueryResult<TResponse[], any>;
-	/**
-	 * If source is not configured to run with count enabled, this will be undefined.
-	 */
-	readonly count: UseQueryResult<number, any> | undefined;
-
-	pagination(): PaginationConfig | false | undefined;
 
 	hasData(): boolean;
 
