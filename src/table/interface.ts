b210e3c08e4f1882fcdab8f8e3bae3e679d81bbc
Marek Hanzal <marek.hanzal@x32.cz>
2022-08-17 13:51:27 +0200

    Common: Version bump

--- src/table/interface.ts
+++ src/table/interface.ts
@@ -1,5 +1,4 @@
-import {IQueryFilter, ISourceContext, ISourceItem, ISourceQuery} from "@leight-core/api";
-import type {FilterValue} from "antd/es/table/interface";
+import {ISourceContext, ISourceItem} from "@leight-core/api";
 import type {ColumnProps} from "antd/lib/table";
 import {ReactNode} from "react";
 
@@ -15,13 +14,4 @@ export interface IITableChildren<TSourceContext extends ISourceContext<any>> {
 
 export interface ITableChildrenCallback<TSourceContext extends ISourceContext<any>> {
 	(children: IITableChildren<TSourceContext>): ReactNode;
-}
-
-export interface ITableToFilter<TSourceContext extends ISourceContext<any>> {
-	readonly filters: Record<keyof ISourceItem<TSourceContext>, FilterValue | null>;
-	readonly current?: IQueryFilter<ISourceQuery<TSourceContext>> | null;
-}
-
-export interface ITableToFilterCallback<TSourceContext extends ISourceContext<any>> {
-	(filters: ITableToFilter<TSourceContext>): IQueryFilter<ISourceQuery<TSourceContext>> | null;
 }
