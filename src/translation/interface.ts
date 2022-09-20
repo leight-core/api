cbe70b3e6f9a2a7deabce21a8f8633ac2c634bef
Marek Hanzal <marek.hanzal@x32.cz>
2022-09-20 15:12:48 +0200

    Common: Version bump

--- src/translation/interface.ts
+++ src/translation/interface.ts
@@ -1,5 +1,6 @@
 import {IQueryHook} from "@leight-core/api";
 import {i18n} from "i18next";
+import {ReactNode} from "react";
 
 export interface II18NextContext {
 	readonly i18next: i18n;
@@ -21,3 +22,18 @@ export interface ITranslationBundle {
 }
 
 export type ITranslationsQuery = IQueryHook<void, ITranslationBundle>;
+
+export interface ITranslationProps {
+	/**
+	 * Should translate only if "text" is string.
+	 */
+	readonly text?: ReactNode;
+	/**
+	 * Namespace used as a prefix for "text" (by dots, for example "namespace.text")
+	 */
+	readonly namespace?: string;
+	/**
+	 * Values used in translation placeholders.
+	 */
+	readonly values?: Record<string, any>;
+}
