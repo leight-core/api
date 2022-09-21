32dbaf9e440a58720e897201594fc0e67c8563b3
Marek Hanzal <marek.hanzal@x32.cz>
2022-09-21 02:53:32 +0200

    Common: Version bump

--- src/selection/interface.ts
+++ src/selection/interface.ts
@@ -1,9 +1,25 @@
 export type ISelectionType = "none" | "single" | "multi";
 
 export interface ISelection<TSelection> {
+	/**
+	 * Single (if any) selected item.
+	 */
 	single: TSelection | undefined;
+	/**
+	 * Selected IDs.
+	 */
 	selected: string[];
+	/**
+	 * Selected items indexed by an ID.
+	 */
 	items: { [index in string]: TSelection };
+	/**
+	 * Just an array of selected items.
+	 */
+	selection: TSelection[];
+	/**
+	 * Quite talkative, isn't?
+	 */
 	isEmpty: boolean;
 }
 
