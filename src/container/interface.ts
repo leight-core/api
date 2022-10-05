52c1bb3be93774d0df74b3dcefe7860be0c3e21a
Marek Hanzal <marek.hanzal@x32.cz>
2022-10-05 20:21:46 +0200

    Common: Version bump

--- src/container/interface.ts
+++ src/container/interface.ts
@@ -1,7 +1,10 @@
+import {IFileSource, ISource} from "@leight-core/api";
+
 /**
  * Callback used to obtain a service from a Container.
  */
 export type IContainerCallback<TService, T> = (service: TService) => Promise<T>;
 
-export interface IContainer {
+export interface IContainer<TFileSource extends IFileSource<any, any>> {
+	useFileSource<T>(callback: IContainerCallback<TFileSource, T>, source?: ISource<any, any, any>): Promise<T>;
 }
