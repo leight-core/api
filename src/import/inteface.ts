a65ca905eb856b2d198860e145f73dddc83afdbb
Marek Hanzal <marek.hanzal@x32.cz>
2022-10-12 19:53:50 +0200

    Common: ...

--- src/import/inteface.ts
+++ src/import/inteface.ts
@@ -1,5 +1,3 @@
-import {IContainer} from "@leight-core/api";
-
 export interface IImportTabs {
 	tab: string;
 	services: string[];
@@ -10,28 +8,6 @@ export type IImportTranslations = Record<string, string>;
 export interface IImportMeta {
 	tabs: IImportTabs[];
 	translations: IImportTranslations;
-}
-
-export interface IImportBeginEvent {
-}
-
-export interface IImportEndEvent {
-}
-
-export interface IImportHandler<TItem> {
-	container: IContainer;
-
-	begin?(event: IImportBeginEvent): Promise<void>;
-
-	end?(event: IImportEndEvent): Promise<void>;
-
-	handler(item: TItem): Promise<any>;
-}
-
-export type IImportHandlers = Record<string, () => IImportHandler<any>>;
-
-export interface IWithImporters {
-	importers: IImportHandlers;
 }
 
 export interface IImportSource<//
