c020182669c9991bdce82bf5b0dfaf71b58da682
Marek Hanzal <marek.hanzal@x32.cz>
2022-10-12 17:14:37 +0200

    Common: ...

--- src/link/interface.ts
+++ src/link/interface.ts
@@ -1,4 +1,4 @@
-export type IQueryParams = Record<string, string | undefined>;
+export type IQueryParams = Record<string, string | number | undefined>;
 
 export interface IQueryParamsContext<TQueryParams extends IQueryParams | undefined = undefined> {
 	readonly queryParams: TQueryParams;
